a9f0cfc6e881e32fcda642647557b97d2a427932
Rafael Cavasani <rafael.cavasani@gmail.com>
2020-02-18 12:55:05 -0300

    commit 3

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -5,4 +5,4 @@ class PontoTuristicoSerializer(ModelSerializer):
 
     class Meta():
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao')
+        fields = ('id', 'nome', 'descricao', 'atracoes')
